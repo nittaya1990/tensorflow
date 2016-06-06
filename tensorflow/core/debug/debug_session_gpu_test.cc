b804a7413b31d323c2436ee34f9ed893dad9e873
Ubuntu <ubuntu@ip-172-31-13-3.ec2.internal>
2016-06-06 21:09:49 +0000

    tfdb-p2: Chaser: fix callback format

--- tensorflow/core/debug/debug_session_gpu_test.cc
+++ tensorflow/core/debug/debug_session_gpu_test.cc
@@ -91,7 +91,6 @@ TEST_F(DebugSessionGPUMinusAXTest, RunSimpleNetwork) {
       [&mu, &completed_nodes, &output_slots_comp, &is_refs_comp](
           const string& node_name,
           const int output_slot,
-          const int64& completion_timestamp,
           const bool is_ref) {
     mutex_lock l(mu);
     completed_nodes.push_back(node_name);
