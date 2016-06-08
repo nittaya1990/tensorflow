87af79a20f80b1e58f9d6d3f0f6658438a1c94fa
Shanqing Cai <cais@google.com>
2016-06-08 19:59:06 -0400

    tfdb-p2: Chaser: Logic cleanup; Unitialized tensors

--- tensorflow/core/debug/debug_session.cc
+++ tensorflow/core/debug/debug_session.cc
@@ -74,52 +74,52 @@ void DebugSession::CopyTensor(const string& node_name, const int output_slot,
                               const Tensor* src_tensor, OpKernelContext* ctx,
                               CopyDoneCallback copy_done_cb) {
   Device* device = static_cast<Device*>(ctx->device());
+
+  // Create copied tensor on host
   AllocatorAttributes alloc_attrs = ctx->output_alloc_attr(output_slot);
+  Allocator* cpu_allocator = tensorflow::cpu_allocator();
+  Tensor* cpu_tensor =
+      new Tensor(cpu_allocator, src_tensor->dtype(), src_tensor->shape());
 
-  const bool is_tensor_initialized = src_tensor->IsInitialized();
+  // Keep track of the tensors created for copying so that can be freed
+  // later.
+  {
+    mutex_lock l(mu_);
+    host_tensors_.insert(std::make_pair(node_name, cpu_tensor));
+  }
 
-  // Omit uniniatizlied Tensors.
-  if (device->name().find("gpu:") != string::npos && !alloc_attrs.on_host() &&
-      is_tensor_initialized) {
-    // For GPU tensors, copy them to CPU.
-    Allocator* cpu_allocator = tensorflow::cpu_allocator();
-    Tensor* cpu_tensor =
-        new Tensor(cpu_allocator, src_tensor->dtype(), src_tensor->shape());
+  // Determine if the tensor is initialized properly.
+  if (src_tensor->IsInitialized() && DataTypeSize(src_tensor->dtype()) > 0) {
+    // Tensor is initialized: Make copy.
 
-    // Keep track of the tensors created for copying so that can be freed
-    // later.
-    {
-      mutex_lock l(mu_);
-      host_tensors_.insert(std::make_pair(node_name, cpu_tensor));
+    // Determine if the tensor is on device (GPU) or host (CPU).
+    if (device->name().find("gpu:") != string::npos &&
+        !alloc_attrs.on_host()) {
+      // GPU tensors: Copy it to host (CPU).
+      DeviceContext* device_ctxt = ctx->op_device_context();
+
+      // Copy device (e.g., GPU) tensor to host and when done, invoke the
+      // callback.
+      device_ctxt->CopyDeviceTensorToCPU(
+          src_tensor, "TensorCopy", device, cpu_tensor,
+          [node_name, cpu_tensor, copy_done_cb](const Status& s) {
+            if (s.ok()) {
+              copy_done_cb(cpu_tensor);
+            } else {
+              LOG(ERROR) << "Copying of device Tensor " << node_name
+                         << " to CPU failed.";
+            }
+          });
+    } else {
+      // For CPU tensors, copy the source tensor and own the copy, because the
+      // value callback may outlive the life time of the source tensor.
+      cpu_tensor->UnsafeCopyFromInternal(*src_tensor, src_tensor->shape());
+
+      copy_done_cb(cpu_tensor);
     }
-
-    DeviceContext* device_ctxt = ctx->op_device_context();
-
-    // Copy device (e.g., GPU) tensor to host and when done, invoke the
-    // callback.
-    device_ctxt->CopyDeviceTensorToCPU(
-        src_tensor, "TensorCopy", device, cpu_tensor,
-        [node_name, cpu_tensor, copy_done_cb](const Status& s) {
-          if (s.ok()) {
-            copy_done_cb(cpu_tensor);
-          } else {
-            LOG(ERROR) << "Copying of device Tensor " << node_name
-                       << " to CPU failed.";
-          }
-        });
   } else {
-    // For CPU tensors, copy the source tensor and own the copy, because the
-    // value callback may outlive the life time of the source tensor.
-    Tensor* dst_tensor = new Tensor();
-    dst_tensor->UnsafeCopyFromInternal(*src_tensor, src_tensor->shape());
-
-    // Keep track of the copied tensor to be freed later.
-    {
-      mutex_lock l(mu_);
-      host_tensors_.insert(std::make_pair(node_name, dst_tensor));
-    }
-
-    copy_done_cb(dst_tensor);
+    // Tensor is not initialized: No need to copy.
+    copy_done_cb(cpu_tensor);
   }
 }
 
