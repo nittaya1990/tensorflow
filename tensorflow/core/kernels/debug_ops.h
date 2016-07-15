a7288fc1287bab247c10725c59a395650ec52b2e
Shanqing Cai <cais@google.com>
2016-07-15 10:43:46 -0400

    Debug node insertion

--- tensorflow/core/kernels/debug_ops.h
+++ tensorflow/core/kernels/debug_ops.h
@@ -93,6 +93,9 @@ class DebugIdentityOp : public OpKernel {
   }
 
   void Compute(OpKernelContext* context) override {
+    std::cout << "DebugIdentityOp::Compute(): input = "
+              << context->input(0).DebugString() << std::endl;  // DEBUG
+
     context->set_output(0, context->input(0));
   }
 
